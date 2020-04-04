dc4fcc8b9e328aeb9921016c7b2306edb526325e
Crispin Quizo <cqm.cris@gmail.com>
2020-04-04 15:26:28 -0400

    first commit

---
+++ src/main/java/com/app/aforo255/account/ServiceBankaforo255AccountApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankaforo255AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankaforo255AccountApplication.class, args);
+	}
+
+}
